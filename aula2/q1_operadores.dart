271324f4eb8168a9e4f752e33b917cba0a86b549
Isaac <isaacnbgomes@gmail.com>
2023-07-25 10:44:30 -0300

    Create q1_operadores.dart

---
+++ aula2/q1_operadores.dart
@@ -0,0 +1,15 @@
+void main() {
+  int n1 = 4;
+  int n2 = 8;
+  
+  int soma = n1 + n2;
+  print('$n1 + $n2 = $soma');
+  int subtracao = n2 - n1;
+  print('$n2 - $n1 = $subtracao');
+  int multi = n1 * n2;
+  print('$n1 x $n2 = $multi');
+  double div = n1 / n2;
+  print('$n1 / $n2 = $div');
+  int resto = n1 % n2;
+  print('$n1 % $n2 = $resto');
+}
