a9010fe17c48878b8db337a002ac19d72b56dd1c
Isaac <isaacnbgomes@gmail.com>
2023-07-25 11:14:32 -0300

    Create q5_que_dia_eh_hoje.dart

---
+++ aula2/q5_que_dia_eh_hoje.dart
@@ -0,0 +1,27 @@
+void main() {
+  int input = 2;
+  
+  switch(input) {
+    case 1:
+      print('Hoje é segunda-feira');
+      break;
+    case 2:
+      print('Hoje é terça-feira');
+      break;
+    case 3:
+      print('Hoje é quarta-feira');
+      break;
+    case 4:
+      print('Hoje é quinta-feira');
+      break;
+    case 5:
+      print('Hoje é sexta-feira');
+      break;
+    case 6:
+      print('Hoje é sábado');
+      break;
+    case 7:
+      print('Hoje é domingo');
+      break;
+  }
+}
