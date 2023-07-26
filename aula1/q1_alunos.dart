2a009752a3421a7b87bea0cf7316ac7f17fbb09b
Isaac <isaacnbgomes@gmail.com>
2023-07-25 21:01:09 -0300

    Create q1_alunos.dart

---
+++ aula1/q1_alunos.dart
@@ -0,0 +1,17 @@
+void main() {
+    final String nome = 'Isaac Newton Barros Gomes';
+    int idade = 23;
+    bool ehNovato = false;
+    double peso = 70.2;
+    final List<int> notas = [6, 7, 8, 9];
+    const Set<String> disciplinas = {'Algoritmos', 'POO', 'Banco de Dados'};
+    String? endereco = 'Ananindeua, Pará';
+    
+    print('Nome do aluno: $nome');
+    print('Idade: $idade');
+    print('Entrou no ano atual: $ehNovato');
+    print('Peso: $peso');
+    print('Notas: $notas');
+    print('Disciplinas: $disciplinas');
+    print('Endereço: $endereco');
+}
